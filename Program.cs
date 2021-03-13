4ad4efaf67048358b984a3bebf932cd147407c20
3253099643qq.com <3253099643@qq.com>
2021-03-13 16:23:32 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b._2_c_sharp
+{
+    class Program
+    {
+      
+            static void Main(string[] args)
+            {
+                int[] t = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                max_min_ave_sum(t);
+            }
+            public static void max_min_ave_sum(int[] t)
+            {
+                int max = t[0];
+                int min = t[0];
+                double ave;
+                int sum = 0;
+                for (int i = 0; i < t.Length; i++)
+                {
+                    if (t[i] > max)
+                    {
+                        max = t[i];
+                    }
+                    if (t[i] < min)
+                    {
+                        min = t[i];
+                    }
+                    sum = sum + t[i];
+                }
+                ave = sum / t.Length;
+                Console.WriteLine("最大值为：" + max);
+                Console.WriteLine("最小值为：" + min);
+                Console.WriteLine("平均值为：" + ave);
+                Console.WriteLine("和为：" + sum);
+            
+        }
+    }
+}
